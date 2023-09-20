a150c1a95550b76f1c488e5b766b5b952ab76902
Abubakar Javed <abubakarjaved03145005454@gmail.com>
2023-09-21 00:34:05 +0500

    Added basic layout, as well as added basic header setup

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
-import Test from './components/test/test';
+import Header from './components/header/Header';
 
 function App() {
   return (
     <div className="App">
-      <Test/>
+      <Header/>
     </div>
   );
 }
