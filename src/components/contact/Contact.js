04b37404f94959c870b47afc3550596d1d7396c0
Abubakar Javed <abubakarjaved03145005454@gmail.com>
2024-06-15 20:44:51 +0500

    testimonials and Contact responsiveness completed...

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -16,21 +16,27 @@ const Contact = (props) => {
             setContact(CONTACT);
         }
     }, [props.ProfileData])
-    const boxStyle1 = {
-        height: "14rem",
-        width: "16rem",
-        float: "right",
-        border: "1px solid rgb(146, 94, 94)",
-        transition: "background-color 0.3s",
-        textAlign: "center",
-        paddingTop: "1rem"
-    };
-
-    const boxStyle2 = {
-        ...boxStyle1,
-        float: "left",
-        paddingTop: "1.1rem"
-    };
+    const buttonStyle = { width: "11rem", padding: "0.5rem 1.5rem", borderRadius: "10px", fontWeight: "300", fontSize: "medium",marginLeft:"0" }
+    const [boxStyle1, setBoxStyle1] = useState({height: "14rem",width: "16rem",border: "1px solid rgb(146, 94, 94)",transition: "background-color 0.3s",textAlign: "center",paddingTop: "1rem",marginLeft:"auto",marginRight:"auto",marginBottom:"2rem"});
+    const [boxStyle2, setBoxStyle2] = useState({height: "14rem",width: "16rem",border: "1px solid rgb(146, 94, 94)",transition: "background-color 0.3s",textAlign: "center",paddingTop: "1.1rem",marginLeft:"auto",marginRight:"auto"});
+    useEffect(() => {
+        const updateBoxStyle = () => {
+          if (window.innerWidth < 768) { 
+            setBoxStyle1({height: "14rem",width: "16rem",border: "1px solid rgb(146, 94, 94)",transition: "background-color 0.3s",textAlign: "center",paddingTop: "1rem",marginLeft:"auto",marginRight:"auto",marginBottom:"2rem"});
+            setBoxStyle2({height: "14rem",width: "16rem",border: "1px solid rgb(146, 94, 94)",transition: "background-color 0.3s",textAlign: "center",paddingTop: "1.1rem",marginLeft:"auto",marginRight:"auto"});
+          } 
+          else{ 
+            setBoxStyle1({height: "14rem",width: "16rem",float: "right",border: "1px solid rgb(146, 94, 94)",transition: "background-color 0.3s",textAlign: "center",paddingTop: "1rem"});
+            setBoxStyle2({height: "14rem",width: "16rem",float: "left",border: "1px solid rgb(146, 94, 94)",transition: "background-color 0.3s",textAlign: "center",paddingTop: "1.1rem"});
+         }
+        };
+    
+        updateBoxStyle(); 
+        window.addEventListener('resize', updateBoxStyle);
+    
+        return () => window.removeEventListener('resize', updateBoxStyle);
+    
+      }, []);
     const contactTitleStyle = {
         fontSize: "larger",
         fontWeight: "700",
@@ -47,7 +53,7 @@ const Contact = (props) => {
     const contactDetailStyleWhatsapp = {
         fontSize: "small",
         fontFamily: "Arial, Helvetica, sans-serif",
-        color: "white",
+        color: "#b8b7b6",
     }
     const contactDetailStyle = {
         ...contactDetailStyleWhatsapp,
@@ -74,27 +80,27 @@ const Contact = (props) => {
             <h1 className={`text-center title p-5`} id='contactHeading'>Contact</h1>
             <div className="container">
                 <div className="row">
-                    <div className="col-6">
+                    <div className="col-12 col-md-6">
                         <span onMouseEnter={() => handleHover(1)}
                             onMouseLeave={() => handleLeave()}>
                             <Box style={hoveredBox === 1 ? { ...boxStyle1, boxShadow: "0px 0px 40px rgba(186, 10, 10, 0.804)", border: "1px solid white" } : boxStyle1}>
                                 <img className={classes.contactImage} src="email_icon.svg" height="40rem" width="40rem"></img>
                                 <hr className={classes.boxLine}></hr>
-                                <p className={classes.contactTitle} style={hoveredBox === 1 ? { ...contactTitleStyle, color: "white" } : contactTitleStyle}>Email</p>
-                                <p className={classes.contactDetail} style={hoveredBox === 1 ? { ...contactDetailStyle, color: "white" } : contactDetailStyle}>{contact.email}</p>
-                                <Button alt className={classes.contactButton} onClick={openEmail}>Send Email</Button>
+                                <p className={classes.contactTitle} style={contactTitleStyle}>Email</p>
+                                <p className={classes.contactDetail} style={contactDetailStyle}>{contact.email}</p>
+                                <Button alt className={classes.contactButton} styles={buttonStyle} onClick={openEmail}>Send Email</Button>
                             </Box>
                         </span>
                     </div>
-                    <div className="col-6">
+                    <div className="col-12 col-md-6">
                         <span onMouseEnter={() => handleHover(2)}
                             onMouseLeave={() => handleLeave()}>
                             <Box style={hoveredBox === 2 ? { ...boxStyle2, boxShadow: "0px 0px 40px rgba(186, 10, 10, 0.804)", border: "1px solid white" } : boxStyle2}>
                                 <img className={classes.contactImageWhatsapp} src="whatsapp_icon.svg" height="35rem" width="35rem"></img>
                                 <hr className={classes.boxLine}></hr>
-                                <p className={classes.contactTitle} style={hoveredBox === 1 ? { ...contactTitleStyleWhatsapp, color: "white" } : contactTitleStyleWhatsapp}>Whatsapp</p>
-                                <p className={classes.contactDetail} style={hoveredBox === 1 ? { ...contactDetailStyleWhatsapp, color: "white" } : contactDetailStyleWhatsapp}>{contact.whatsapp}</p>
-                                <Button alt className={classes.contactButton} onClick={openWhatsapp}>Send Message</Button>
+                                <p className={classes.contactTitle} style={contactTitleStyleWhatsapp}>Whatsapp</p>
+                                <p className={classes.contactDetail} style={contactDetailStyleWhatsapp}>{contact.whatsapp}</p>
+                                <Button alt className={classes.contactButton} styles={buttonStyle} onClick={openWhatsapp}>Send Message</Button>
                             </Box>
                         </span>
                     </div>
--- src/components/testimonials/Testimonials.js
+++ src/components/testimonials/Testimonials.js
@@ -21,8 +21,32 @@ const Testimonials = (props) => {
     }, [props.ProfileData])
 
 
-    const boxStyle1 = { height: "29rem", width: "27rem", textAlign: "center", overflow: 'hidden' };
-
+    const [boxStyle, setBoxStyle] = useState({ height: "29rem", width: "27rem", textAlign: "center", overflow: 'hidden', marginLeft: "auto", marginRight: "auto",padding:"0"});
+    useEffect(() => {
+        const updateBoxStyle = () => {
+          if (window.innerWidth < 400) { 
+            setBoxStyle({ height: "33rem", width: "15rem", textAlign: "center", overflow: 'hidden', marginLeft: "auto", marginRight: "auto",padding:"0"});
+          } 
+          else if (window.innerWidth < 450){ 
+            setBoxStyle({ height: "29rem", width: "18rem", textAlign: "center", overflow: 'hidden', marginLeft: "auto", marginRight: "auto",padding:"0.2rem"});
+          }
+          else if (window.innerWidth < 576){ 
+            setBoxStyle({ height: "29rem", width: "20rem", textAlign: "center", overflow: 'hidden', marginLeft: "auto", marginRight: "auto",padding:"0.5rem"});
+          }
+          else if (window.innerWidth < 768){ 
+            setBoxStyle({ height: "29rem", width: "24rem", textAlign: "center", overflow: 'hidden', marginLeft: "auto", marginRight: "auto",padding:"1.2rem"});
+          }
+          else{ 
+            setBoxStyle({ height: "27rem", width: "27rem", textAlign: "center", overflow: 'hidden', marginLeft: "auto", marginRight: "auto",padding:"2rem"});
+          }
+        };
+    
+        updateBoxStyle(); 
+        window.addEventListener('resize', updateBoxStyle);
+    
+        return () => window.removeEventListener('resize', updateBoxStyle);
+    
+      }, []);
     useEffect(() => {
         const timerId = setTimeout(() => {
             setCurrentIndex((prevIndex) => (prevIndex === testimonials.length - 1 ? 0 : prevIndex + 1));
@@ -44,16 +68,17 @@ const Testimonials = (props) => {
             <h1 className={`text-center title pb-5`} id='testimonialsHeading'>Testimonials</h1>
             <div className="container">
                 <div className="row text-center">
-                    <div className={`col-3 ${classes.center_vertical} p-5`}><Button className={`${classes.carouselButton} prev`} onClick={handlePrev}>
-                        &#8592;
-                    </Button></div>
-                    <div className={`col-6`}>
+                    <div className={`col-1 col-md-2 col-xl-3 col-xxl-4 ${classes.center_vertical}`}>
+                        <Button className={`${classes.carouselButton} prev`} onClick={handlePrev}>
+                            &#8592;
+                        </Button></div>
+                    <div className={`col-10 col-md-8 col-xl-6 col-xxl-4`}>
                         {testimonials.map((element, index) => (
                             <div
                                 key={index}
                                 className={`${classes.carousel_item} carousel-item  ${currentIndex === index ? 'active' : ''}`}
                             >
-                                <Box style={boxStyle1}>
+                                <Box style={boxStyle}>
                                     <img className={classes.testPic} src={element.pic}></img>
                                     <h2 className={classes.testName}>{element.name}</h2>
                                     <hr className={classes.boxLine}></hr>
@@ -62,7 +87,7 @@ const Testimonials = (props) => {
                             </div>
                         ))}
                     </div>
-                    <div className={`col-3 ${classes.center_vertical} p-5`}>
+                    <div className={`col-1 col-md-2 col-xl-3 col-xxl-4 ${classes.center_vertical}`}>
                         <Button className={`${classes.carouselButton} next`} onClick={handleNext}>
                             &#8594;
                         </Button>
